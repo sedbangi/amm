beb652be786ba5a621f675e34ffab73428cec1ca
sid_naik <siddharth@equalto.io>
2024-09-09 20:12:29 +0200

    made historical blocks a constant variable

--- damm/src/PriorityFeeAndPriceReturnVolatilitySimulator.sol
+++ damm/src/PriorityFeeAndPriceReturnVolatilitySimulator.sol
@@ -4,9 +4,10 @@ import "./MathLibrary.sol";
 
 contract PriorityFeeAndPriceReturnVolatilitySimulator {
     using MathLibrary for uint256;
-    uint256[200] public priorityFees;
-    uint256[200] public prices;
-    uint256[200] public blockNumbers;
+    uint constant historicalBlocks = 200;
+    uint256[historicalBlocks] public priorityFees;
+    uint256[historicalBlocks] public prices;
+    uint256[historicalBlocks] public blockNumbers;
     uint256 public index;
 
     constructor() {
@@ -18,24 +19,24 @@ contract PriorityFeeAndPriceReturnVolatilitySimulator {
             priorityFees[index] = priorityFee;
             prices[index] = price;
             blockNumbers[index] = block.number;
-            index = (index + 1) % 200;
+            index = (index + 1) % historicalBlocks;
         }
     }
 
-    function calculateVolatility(uint256[200] memory data) internal pure returns (uint256) {
+    function calculateVolatility(uint256[historicalBlocks] memory data) internal pure returns (uint256) {
         uint256 mean = 0;
-        for (uint256 i = 0; i < 200; i++) {
+        for (uint256 i = 0; i < historicalBlocks; i++) {
             mean += data[i];
         }
         mean /= 200;
 
         uint256 variance = 0;
-        for (uint256 i = 0; i < 200; i++) {
+        for (uint256 i = 0; i < historicalBlocks; i++) {
             variance += (data[i] - mean) ** 2;
         }
         variance /= 200;
 
-        return sqrt(variance);
+        return variance.sqrt();
     }
 
     function getPriorityFeeVolatility() public view returns (uint256) {
