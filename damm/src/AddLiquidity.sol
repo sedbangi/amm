b9e858bac15df8c4fd9cc8046a21f3ae4a689c74
sid_naik <siddharth@equalto.io>
2024-09-09 19:38:38 +0200

    1. Oracle order book pressue simulation done, 2. Add Liquity simply a template, 3. Standard Deviation efficient calculation: template

---
+++ damm/src/AddLiquidity.sol
@@ -0,0 +1,8 @@
+pragma solidity 0.8.26;
+
+
+contract AddLiquidity {
+    uint128 public LowerTick;
+    uint128 public UpperTick;
+    
+}
--- damm/src/DammOracle.sol
+++ damm/src/DammOracle.sol
@@ -2,10 +2,14 @@
 pragma solidity 0.8.26;
 
 import {console} from "forge-std/console.sol";
+import {FullMath} from "lib/v4-periphery/lib/v4-core/src/libraries/FullMath.sol";
 
 contract DammOracle {
     uint256 public OFF_CHAIN_MID_PRICE_ETH_USDT = 2200;
     uint256 public HALF_SPREAD = 5000;
+    uint256 constant HUNDRED_PERCENT = 1_000_000;
+    uint256 constant SCALING_FACTOR = 10**18;
+    uint256 public SqrtX96Price;
     
 
     /**
@@ -15,23 +19,41 @@ contract DammOracle {
         return OFF_CHAIN_MID_PRICE_ETH_USDT;
     }
 
+    /*
+    function sqrt(uint x) returns (uint y) {
+        uint z = (x + 1) / 2;
+        y = x;
+        while (z < y) {
+            y = z;
+            z = (x / z + z) / 2;
+        }
+        return y;
+    }
+    */
+
     /**
      * Returns the simulated orderbookpressure
      */
     function getOrderBookPressure() public view returns (uint256) {
         uint256 bidSize = random(1, 1000);
-        console.logUint(bidSize);
-        uint256 bidPrice = OFF_CHAIN_MID_PRICE_ETH_USDT * (1000 - HALF_SPREAD) / 1000;
-        console.logUint((bidPrice));
-        // uint256 askPrice = OFF_CHAIN_MID_PRICE_ETH_USDT * (1000 + HALF_SPREAD) / 1000;
-        // uint256 askSize = random(1, 1000);
+        // console.logUint("bid size");
+        console.log("bid size:", bidSize);
+        uint256 bidPrice = OFF_CHAIN_MID_PRICE_ETH_USDT * (HUNDRED_PERCENT - HALF_SPREAD) / HUNDRED_PERCENT;
+        // console.logUint("bid price");
+        console.log("bid price:", bidPrice);
+        uint256 askPrice = OFF_CHAIN_MID_PRICE_ETH_USDT * (HUNDRED_PERCENT + HALF_SPREAD) / HUNDRED_PERCENT;
+        // console.logUint("ask price");
+        console.log("ask price:", askPrice);
+        uint256 askSize = random(1, 1000);
+        // console.logUint("ask size");
+        console.log("ask size:", askSize);
 
         // while (askSize == bidSize) {
         //     askSize = random(1, 1000);
         // }
 
         // uint256 bidValue = bidSize * bidPrice;
-        // uint256 askValue = askSize * askPrice;
+        // uint256 askValue = askSize * askPrice;f
         // return (askValue - bidValue) * 1000 / (askValue + bidValue);
         return 5000;
     }
