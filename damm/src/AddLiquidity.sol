97338c417ff8ea8a75d9a28e0d80292a72e33a9b
sid_naik <siddharth@equalto.io>
2024-09-14 12:08:01 +0200

    intermediate push to correct all the errors

--- damm/src/AddLiquidity.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity 0.8.26;
-
-
-contract AddLiquidity {
-    uint128 public LowerTick;
-    uint128 public UpperTick;
-    
-}
--- damm/test/test.t.sol
+++ damm/test/test.t.sol
@@ -1,29 +1,47 @@
-pragma solidity ^0.8.26;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
 
-import "forge-std/Test.sol";
-import "../src/DammHook.sol";
-import "../src/MockDammOracle.sol";
+import {console} from "forge-std/console.sol";
+import {Test} from "forge-std/Test.sol";
+import {IPoolManager} from "v4-core/interfaces/IPoolManager.sol";
+import {DammHook} from "../src/DammHook.sol";
+import {DammOracle} from "../src/DammOracle.sol";
 
 contract DammHookTest is Test {
     DammHook dammHook;
-    MockDammOracle mockDammOracle;
+    DammOracle dammOracle;
+
+    struct NewHookData {
+        address sender;
+        bytes hookData;
+    }
+
+    struct BeforeSwapDelta {
+        uint256 delta0;
+        uint256 delta1;
+    }
 
     function setUp() public {
         // Deploy the mock oracle
-        mockDammOracle = new MockDammOracle();
+        dammOracle = new DammOracle();
         // Deploy the DammHook contract with the mock oracle address
-        dammHook = new DammHook(address(mockDammOracle));
+        dammHook = new DammHook(address(dammOracle));
     }
 
     function testBeforeSwap() public {
         // Mock data
         address sender = address(this);
-        PoolKey memory key = PoolKey({
+        IPoolManager.PoolKey memory key = IPoolManager.PoolKey({
             // Initialize with mock data
+            token0: address(0),
+            token1: address(0),
+            fee: 0
         });
         IPoolManager.SwapParams memory params = IPoolManager.SwapParams({
             amountSpecified: 1000,
-            // Initialize other params
+            sqrtPriceLimitX96: 0,
+            recipient: address(0),
+            deadline: block.timestamp
         });
         bytes memory hookData = abi.encode(NewHookData({
             sender: sender,
@@ -31,7 +49,7 @@ contract DammHookTest is Test {
         }));
 
         // Set mock oracle data
-        mockDammOracle.setOrderBookPressure(2000);
+        dammOracle.setOrderBookPressure(2000);
 
         // Call the beforeSwap function
         (bytes4 resultBytes4, BeforeSwapDelta memory resultDelta, uint24 resultFee) = dammHook.beforeSwap(
@@ -44,12 +62,14 @@ contract DammHookTest is Test {
         // Assertions
         bytes4 expectedBytes4 = 0x12345678; // Replace with expected value
         BeforeSwapDelta memory expectedDelta = BeforeSwapDelta({
-            // Initialize with expected values
+            delta0: 0,
+            delta1: 0
         });
         uint24 expectedFee = 1000; // Replace with expected value
 
         assertEq(resultBytes4, expectedBytes4, "Incorrect bytes4 returned");
-        assertEq(resultDelta, expectedDelta, "Incorrect BeforeSwapDelta returned");
+        assertEq(resultDelta.delta0, expectedDelta.delta0, "Incorrect BeforeSwapDelta delta0 returned");
+        assertEq(resultDelta.delta1, expectedDelta.delta1, "Incorrect BeforeSwapDelta delta1 returned");
         assertEq(resultFee, expectedFee, "Incorrect uint24 returned");
     }
 }
